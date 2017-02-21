9d6524911f2e0e7b5289379f5d86e7a91700560b
Maia Werbos <mwerbos@google.com>
2017-02-21 09:42:56 -0800

    Open individual sticker packs from preview

--- src/components/views/rooms/StickerBrowser.js
+++ src/components/views/rooms/StickerBrowser.js
@@ -2,13 +2,14 @@ var React = require('react');
 
 import UserSettingsStore from '../../../UserSettingsStore';
 
+import StickerPack from './StickerPack.js';
 var MatrixClientPeg = require('../../../MatrixClientPeg');
-
-var StickerPack = require('./StickerPack');
+var StickerPackPreview = require('./StickerPackPreview');
 
 export default class StickerBrowser extends React.Component {
     constructor(props){
         super(props);
+				this.state = {openPack: null};
         const packUrls = UserSettingsStore.getSyncedSetting('StickerBrowser.PackUrls',[])
         Promise.all(
             packUrls.map(mxcUrl => {
@@ -16,18 +17,28 @@ export default class StickerBrowser extends React.Component {
                 return fetch(url).then(response => response.json())
             })
         ).then(stickerPacks => this.setState({packs: stickerPacks}));
+				this.setOpenPack = this.setOpenPack.bind(this);
+				this.clearOpenPack = this.clearOpenPack.bind(this);
     }
+		setOpenPack = (event) => {
+			this.setState({openPack: event.target.name});
+		}
+		clearOpenPack = () => {
+			this.setState({openPack: null});
+		}
     render() {
         if(!this.props.visible) return false;
-        return (<div>
-            {this.state.packs.map(pack => 
-                <StickerPack
-                    title = {pack.title}
-                    stickers = {pack.stickers}
-                    room = {this.props.room}
-                />
-            )}
-        </div>);
+				if (this.state.openPack === null) {
+					return <StickerPackPreview
+									packs={this.state.packs}
+									setOpenPack={this.setOpenPack} />
+				} else {
+						let pack = this.state.packs[this.state.openPack]
+						return <StickerPack
+										title = {pack.title}
+										stickers = {pack.stickers}
+										room = {this.props.room}
+										clearOpenPack = {this.clearOpenPack} />
+				}
     }
-
 }
--- src/components/views/rooms/StickerPack.js
+++ src/components/views/rooms/StickerPack.js
@@ -2,31 +2,35 @@ var React = require('react');
 
 var MatrixClientPeg = require('../../../MatrixClientPeg');
 
-export default class StickerPack extends React.Component {
-    render() {
-        let stickerPreviews = this.props.stickers.map(sticker => {
-            const imageUrl = MatrixClientPeg.get().mxcUrlToHttp(sticker.url);
-            return <img
-                    src={imageUrl}
-                    key={imageUrl}
-                    alt={sticker.emoji}
-                    width="64"
-                    height="64"
-                    onClick={()=> {
-                        const content = {
-                            body: sticker.emoji,
-                            msgtype: "m.image",
-                            url: sticker.url
-                        };
-                        MatrixClientPeg.get().sendMessage(this.props.room.roomId, content);
-                    }}
-                />
-        });
-        return (
-            <div>
-                <div> {this.props.title} </div>
-                <div> {stickerPreviews} </div>
-            </div>
-        )
-    }
+function StickerPack (props) {
+		let stickerPreviews = props.stickers.map(sticker => {
+				const imageUrl = MatrixClientPeg.get().mxcUrlToHttp(sticker.url);
+				return <img
+								src={imageUrl}
+								key={imageUrl}
+								alt={sticker.emoji}
+								width="64"
+								height="64"
+								style={{cursor: 'pointer'}}
+								onClick={()=> {
+										const content = {
+												body: sticker.emoji,
+												msgtype: "m.image",
+												url: sticker.url
+										};
+										MatrixClientPeg.get().sendMessage(props.room.roomId, content);
+								}}
+						/>
+		});
+		return (
+			<div>
+					<div>
+							<a onClick={props.clearOpenPack} style={{cursor: 'pointer'}}>
+									Sticker Packs
+							</a> &gt; <b>{props.title}</b></div>
+					<div> {stickerPreviews} </div>
+			</div>
+		);
 }
+
+module.exports = StickerPack;
