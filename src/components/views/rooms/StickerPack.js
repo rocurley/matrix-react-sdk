45771833034a0844ff3c785e6aba9e1eee6bdd1b
Roger Curley <rocurley@gmail.com>
2017-03-01 19:36:28 -0800

    unjanked tabs

--- src/components/views/rooms/StickerPack.js
+++ src/components/views/rooms/StickerPack.js
@@ -3,34 +3,34 @@ var React = require('react');
 var MatrixClientPeg = require('../../../MatrixClientPeg');
 
 function StickerPack (props) {
-		let stickerPreviews = props.stickers.map(sticker => {
-				const imageUrl = MatrixClientPeg.get().mxcUrlToHttp(sticker.url);
-				return <img
-								src={imageUrl}
-								key={imageUrl}
-								alt={sticker.emoji}
-								width="64"
-								height="64"
-								style={{cursor: 'pointer'}}
-								onClick={()=> {
-										const content = {
-												body: sticker.emoji,
-												msgtype: "m.image",
-												url: sticker.url
-										};
-										MatrixClientPeg.get().sendMessage(props.room.roomId, content);
-								}}
-						/>
-		});
-		return (
-			<div>
-					<div>
-							<a onClick={props.clearOpenPack} style={{cursor: 'pointer'}}>
-									Sticker Packs
-							</a> &gt; <b>{props.title}</b></div>
-					<div> {stickerPreviews} </div>
-			</div>
-		);
+    let stickerPreviews = props.stickers.map(sticker => {
+        const imageUrl = MatrixClientPeg.get().mxcUrlToHttp(sticker.url);
+        return <img
+                src={imageUrl}
+                key={imageUrl}
+                alt={sticker.emoji}
+                width="64"
+                height="64"
+                style={{cursor: 'pointer'}}
+                onClick={()=> {
+                    const content = {
+                        body: sticker.emoji,
+                        msgtype: "m.image",
+                        url: sticker.url
+                    };
+                    MatrixClientPeg.get().sendMessage(props.room.roomId, content);
+                }}
+            />
+    });
+    return (
+      <div>
+          <div>
+              <a onClick={props.clearOpenPack} style={{cursor: 'pointer'}}>
+                  Sticker Packs
+              </a> &gt; <b>{props.title}</b></div>
+          <div> {stickerPreviews} </div>
+      </div>
+    );
 }
 
 module.exports = StickerPack;
